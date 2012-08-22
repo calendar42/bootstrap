1b98b65e0011469f0f3ec25aef1dcd8be0d4d82b
Dennis Koks <mail@denniskoks.com>
2012-08-22 14:31:42 +0200

    popover styling implementation C42

--- js/bootstrap-popover.js
+++ js/bootstrap-popover.js
@@ -92,7 +92,7 @@
   $.fn.popover.defaults = $.extend({} , $.fn.tooltip.defaults, {
     placement: 'right'
   , content: ''
-  , template: '<div class="popover"><div class="arrow"></div><div class="popover-inner"><h3 class="popover-title"></h3><div class="popover-content"><p></p></div></div></div>'
+  , template: '<div class="popover"><div class="arrow"></div><div class="popover-inner"><h4 class="popover-title"></h4><div class="popover-content"><p></p></div></div></div>'
   })
 
 }(window.jQuery);
